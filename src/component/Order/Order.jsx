147f574400568d81eb3b1cdbb671b5cca3bed72b
Bappy <bappymodok0011@gmail.com>
2023-11-28 22:31:52 +0600

    remove singel card data

--- src/component/Order/Order.jsx
+++ src/component/Order/Order.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OrderCard from '../OrderCard/OrderCard';
 import { useLoaderData } from 'react-router-dom';
 import SinelOrder from '../SingelOrder/SinelOrder';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Order = () => {
     const data = useLoaderData();
-    // console.log(data);
+    const [delet,setDelet] = useState(data)
+
+
+    const remobeData = (id) =>{
+        const remaningData = delet.filter(d => d.id !== id)
+        setDelet(remaningData);
+        removeFromDb(id)
+
+    }
     return (
         <div className='max-w-[1240px]  mx-auto'>
-            <div className='grid md:grid-cols-[1fr,1fr] md:p-20 p-5 gap-10'>
+            <div className='grid md:grid-cols-[1fr,1fr] md:p-20 p-5 gap-10 '>
 
            <div className='p-5'>
                 {
-                    data.map(d => <SinelOrder 
+                    delet.map(d => <SinelOrder 
                     key = {d.id}
                     data  = {d}
+                    remobeData={remobeData}
                     ></SinelOrder> )
                 }
            </div>
 
                 <div className='bg-orange-200 p-5 rounded-xl'>
-                    <OrderCard cart={[]}></OrderCard>
+                    <OrderCard cart={delet}></OrderCard>
                 </div>
 
             </div>
--- src/component/SingelOrder/SinelOrder.jsx
+++ src/component/SingelOrder/SinelOrder.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
 import { TrashIcon } from '@heroicons/react/24/solid'
 
-const SinelOrder = ({ data }) => {
-    console.log(data);
+const SinelOrder = ({ data,remobeData }) => {
+    // console.log(remobeData);
     const { img, name, price, shipping } = data;
+    // const {remobeData} = remobeData;
+
+    
+    
+
+  
     return (
-        <div className='flex justify-between items-center mb-2'>
-            <div className='flex gap-2'>
+        <div className='flex justify-between items-center mb-2 border-2 p-2 rounded'>
+            <div className='flex gap-2 '>
                 <img className='w-[90px] rounded' src={img} alt="" />
                 <div>
                     <p>{name}</p>
@@ -17,7 +23,7 @@ const SinelOrder = ({ data }) => {
             </div>
 
             <Link className='bg-red-200 rounded-full p-2'>
-                <TrashIcon className="h-6 w-6 text-red-400" />
+                <TrashIcon onClick={()=>remobeData(data.id)} className="h-6 w-6 text-red-400" />
             </Link>
         </div>
     );
